0cfbf5d8f9e0eb04657e805b1d608008966e7ed0
Yashveer singh <yashveersingh30401@gmail.com>
2023-04-16 21:50:32 +0530

    1. Write my first few test cases

---
+++ app/src/test/java/com/material/service/calculator/RegistrationUtilTest.kt
@@ -0,0 +1,73 @@
+package com.material.service.calculator
+
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class RegistrationUtilTest{
+
+    @Test
+    fun `empty username return false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "",
+            "Hello",
+            "Hello"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `valid username and correctly repeated passwords true`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philip",
+            "Hello343",
+            "Hello343"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `Username already exists return false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "Hello",
+            "Hello"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philip",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password repeated incorrectly returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philip",
+            "Hello1",
+            "Hello"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `password contains less than 2 digits returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philip",
+            "Hdfs3",
+            "Hdfs3"
+        )
+        assertThat(result).isFalse()
+    }
+
+
+
+
+
+}
