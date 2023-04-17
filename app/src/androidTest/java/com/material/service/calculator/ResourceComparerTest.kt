d3c4675537ed578de408b9a753913e9a73e6e8ee
Yashveer singh <yashveersingh30401@gmail.com>
2023-04-17 16:48:06 +0530

    Testing context done

--- app/src/androidTest/java/com/material/service/calculator/ResourceComparerTest.kt
+++ app/src/androidTest/java/com/material/service/calculator/ResourceComparerTest.kt
@@ -1,9 +1,32 @@
 package com.material.service.calculator
 
-import org.junit.Assert.*
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+
+import org.junit.Test
 
 class ResourceComparerTest{
 
+    private lateinit var resourceComparer:ResourceComparer
 
+    @Before
+    fun setup(){
+        resourceComparer = ResourceComparer()
+    }
+    @Test
+    fun stringResourceSameAsGivenString_returnsTrue(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context,R.string.testingString,"Android")
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun stringResourceDifferentAsGivenString_returnsFalse(){
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        val result = resourceComparer.isEqual(context,R.string.testingString,"Fsfas")
+        assertThat(result).isFalse()
+    }
 
 }
