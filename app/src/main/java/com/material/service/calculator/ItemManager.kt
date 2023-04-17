55ecefcf619df342389c19a689eeb904cecb76a6
Yashveer singh <yashveersingh30401@gmail.com>
2023-04-17 16:56:47 +0530

    Creating item manager list with testing

---
+++ app/src/main/java/com/material/service/calculator/ItemManager.kt
@@ -0,0 +1,11 @@
+package com.material.service.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemManager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_manager)
+    }
+}
