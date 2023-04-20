39264d73c384800acbfde7933e17687671ddcf1c
Yashveer singh <yashveersingh30401@gmail.com>
2023-04-20 21:06:36 +0530

    ShoppingRepositoryInterface.kt created

---
+++ app/src/main/java/com/material/service/calculator/repositories/ShoppingRepository.kt
@@ -0,0 +1,8 @@
+package com.material.service.calculator.repositories
+
+import com.material.service.calculator.RoomDB.ShoppingDao
+import com.material.service.calculator.api.PixabayApi
+
+class ShoppingRepository(private val shoppingDao: ShoppingDao, private val  pixabayApi: PixabayApi) {
+
+}
---
+++ app/src/main/java/com/material/service/calculator/repositories/ShoppingRepositoryInterface.kt
@@ -0,0 +1,19 @@
+package com.material.service.calculator.repositories
+
+import com.material.service.calculator.RoomDB.ShoppingItem
+import com.material.service.calculator.model.ImageResponse
+import com.material.service.calculator.other.NetworkResult
+import kotlinx.coroutines.flow.Flow
+
+interface ShoppingRepositoryInterface {
+
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+    fun observeAllShoppingItem(): Flow<List<ShoppingItem>>
+
+    fun observeTotalPrice():Flow<Float>
+
+    suspend fun searchForImage(imageQuery:String):NetworkResult<ImageResponse>
+}
