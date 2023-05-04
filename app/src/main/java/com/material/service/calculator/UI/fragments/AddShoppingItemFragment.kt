e47f48b8e4159ac3c52eaa6684e0962ba649620a
Yashveer singh <yashveersingh30401@gmail.com>
2023-05-04 15:58:38 +0530

    inserting done

--- app/src/main/java/com/material/service/calculator/UI/fragments/AddShoppingItemFragment.kt
+++ app/src/main/java/com/material/service/calculator/UI/fragments/AddShoppingItemFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.material.service.calculator.R
+import com.material.service.calculator.RoomDB.ShoppingItem
 import com.material.service.calculator.adapter.ItemAdapter
 import com.material.service.calculator.databinding.FragmentAddShoppingItemBinding
 import com.material.service.calculator.viewmodel.ShoppingViewModel
@@ -23,8 +25,18 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAddShoppingItemBinding.bind(view)
 
+        binding.btnAddItemToDB.setOnClickListener {
+            addItemToDb()
+        }
 
+    }
 
+    private fun addItemToDb() {
+        val itemName = binding.etItemName.text.toString()
+        val itemAmount = binding.etAmount.text.toString().toInt()
+        val itemPrice=  binding.etAmountperItem.text.toString().toFloat()
+        val imageUrl = binding.etItemImageUrl.text.toString()
+        viewModel.insertShoppingItemIntoDb(ShoppingItem(itemName,itemAmount,itemPrice,imageUrl))
     }
 
 
